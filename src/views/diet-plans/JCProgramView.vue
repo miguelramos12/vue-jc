945a131c071e5adf98146972ccf1cd966a625623
andrew borzillo <dzillo1@gmail.com>
2025-05-30 13:55:21 -0400

    edits added program select menu type

--- src/views/diet-plans/JCProgramView.vue
+++ src/views/diet-plans/JCProgramView.vue
@@ -2,6 +2,7 @@
     import DefaultBreadcrumb from '@/components/breadcrumbs/DefaultBreadcrumb.vue';
     import { ref } from 'vue';
     import DietPlansHero from '@/components/diet-plans/DietPlansHero.vue';
+    import SelectMenuType from '@/components/diet-plans/SelectMenuType.vue';
 
     const pageBreadcrumbs = ref([
         { label: 'Home', route: '/'},
@@ -14,6 +15,7 @@
     <main id="content">
         <DefaultBreadcrumb :breadcrumbs="pageBreadcrumbs" />
         <DietPlansHero />
+        <SelectMenuType />
     </main>
 </template>
 
