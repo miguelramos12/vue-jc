943b0e93d9f9d1df55e11eb17188f8db4ef1eea4
andrew borzillo <dzillo1@gmail.com>
2025-06-05 14:18:47 -0400

    edits diet-plans program

--- src/views/diet-plans/JCProgramView.vue
+++ src/views/diet-plans/JCProgramView.vue
@@ -4,6 +4,7 @@
     import DietPlansHero from '@/components/diet-plans/DietPlansHero.vue';
     import SelectMenuType from '@/components/diet-plans/SelectMenuType.vue';
     import SelectShipmentType from '@/components/diet-plans/SelectShipmentType.vue';
+    import PriceBlock from '@/components/diet-plans/PriceBlock.vue';
 
     const pageBreadcrumbs = ref([
         { label: 'Home', route: '/'},
@@ -18,6 +19,7 @@
         <DietPlansHero />
         <SelectMenuType />
         <SelectShipmentType />
+        <PriceBlock />
     </main>
 </template>
 
