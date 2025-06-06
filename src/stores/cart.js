ac7692b89dfa6d284b886ea682a33a3004ee4133
andrew borzillo <dzillo1@gmail.com>
2025-06-06 11:05:02 -0400

    more actions for cart object

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -9,6 +9,20 @@ export const useCartStore = defineStore('cart', {
     addItem(item) {
       this.items.push(item);
     },
-
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+    clearCart() {
+      this.items = [];
+      this.total = 0;
+    },
+    updateItemQuantity(index, quantity) {
+      if (this.items[index]) {
+        this.items[index].quantity = quantity;
+      }
+    },
+    calculateTotal() {
+      this.total = this.items.reduce((sum, item) => sum + (item.price * (item.quantity || 1)), 0);
+    },
   }
 });
